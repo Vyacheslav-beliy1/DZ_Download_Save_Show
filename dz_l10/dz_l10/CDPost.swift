50422f5cbd27e690560154387ccbafa20d4412b1
Vyacheslav-beliy1 <mister.white.2013@gmail.com>
2017-08-06 01:01:04 +0300

    + DB, UI models;

---
+++ dz_l10/dz_l10/CDPost.swift
@@ -0,0 +1,39 @@
+//
+//  CDPost.swift
+//  dz_l10
+//
+//  Created by bws2007 on 06.08.17.
+//  Copyright © 2017 bws2007. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CDPost: NSManagedObject {
+    class func findOrCreate(with thePost:Post,in context:NSManagedObjectContext ) throws -> CDPost {
+        
+        let request: NSFetchRequest<CDPost> = CDPost.fetchRequest()
+        request.predicate = NSPredicate(format: "identifier == %@", thePost.identifier)
+        
+        do {
+            let matches = try context.fetch(request)
+            if matches.count > 0
+            {
+                assert(matches.count > 1, "Someohw therare not ONE object with identifier")
+                return matches[0]
+            }
+        } catch {
+            //fail error and crash
+            throw error
+        }
+        
+        let cdPost = CDPost(context: context)
+        cdPost.identifier = thePost.identifier
+        cdPost.userID = thePost.userID
+        cdPost.title = thePost.title
+        cdPost.body = thePost.body
+        
+        return cdPost
+
+    }
+}
---
+++ dz_l10/dz_l10/Post.swift
@@ -0,0 +1,52 @@
+//
+//  Post.swift
+//  dz_l10
+//
+//  Created by bws2007 on 06.08.17.
+//  Copyright © 2017 bws2007. All rights reserved.
+//
+
+import Foundation
+
+class Post
+{
+    var userID: Int64
+    var identifier: Int64
+    var title: String?
+    var body: String?
+    
+    init(userID: Int64,identifier: Int64,title: String?,body: String?) {
+        self.userID = userID
+        self.identifier = identifier
+        self.title = title
+        self.body = body
+    }
+}
+
+// MARK: JSON extension
+extension Post
+{
+    class func initFrom(json:[String:Any]) -> Post?
+    {
+        guard let userID = json["userId"] as? Int64,
+            let id = json["id"] as? Int64
+            else { return nil}
+        
+        return Post(userID: userID,
+                    identifier: id,
+                    title: json["title"] as? String,
+                    body: json["body"] as? String)
+    }
+}
+
+// MARK: CoreData extension
+extension Post
+{
+    class func initFrom(cdPost:CDPost) -> Post
+    {
+       return Post(userID: cdPost.userID,
+                   identifier: cdPost.identifier,
+                   title: cdPost.title,
+                   body: cdPost.body)
+    }
+}
